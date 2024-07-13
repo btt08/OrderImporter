2ace86e2ae090beaf5c49de903d0a4c8f3aea38a
btt08 <borjattrapiella@gmail.com>
2024-07-13 15:58:03 +0200

    Creación de resumen y exportación de datos insertados a un nuevo fichero

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,17 @@
-import Tools.DataTreatment;
+import Tools.Tools;
 
 import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.sql.*;
-
-import static java.lang.System.exit;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * TODO
- * -Hacer el resumen tras insertar.
- * -Exportar a un nuevo fichero
  * -Crear una GUI o algo para mostrar el resumen
  */
 
@@ -23,60 +22,154 @@ public class App {
   private static final String DB_URL = "jdbc:mysql://localhost/prueba_basica";
   private static final String DB_USER = "root";
   private static final String DB_PWD = "";
+  private static File importFile;
+  private static File exportFile;
   
   public static void main (String[] args) {
-//    File importFile = DataTreatment.selectFile();
-//
-//    if (importFile == null) {
-//      JOptionPane.showMessageDialog(
-//              null,
-//              "No se seleccionó un archivo válido. Saliendo del programa",
-//              "Alerta", JOptionPane.WARNING_MESSAGE);
-//      exit(1);
-//    }
-    
     try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD)) {
-//      insertRecords(conn, importFile);
-      generateSummary(conn);
+      importFile = Tools.selectImportFile();
+      exportFile = Tools.selectSaveFile();
+      
+      if (importFile == null || exportFile == null) {
+        JOptionPane.showMessageDialog(
+                null,
+                "No se seleccionó un archivo válido. Saliendo del programa",
+                "Alerta", JOptionPane.WARNING_MESSAGE);
+        System.exit(1);
+      }
+      
+      conn.setAutoCommit(false);
+      
+      // Guardamos id mínimo y máximo para identificar lo insertado
+      Integer[] ids = insertRecords(conn);
+      
+      if (ids == null) {
+        System.out.println("Todos los registros son duplicados o el fichero estaba vacío");
+        System.exit(0);
+      }
+      System.out.println("Creando resumen de los datos\n\n");
+      generateSummary(conn, ids);
+      
+      exportToFile(conn, ids);
+      
+      conn.setAutoCommit(true);
+      System.out.println("Finalizada transacción. Datos insertados en la BD");
+      System.out.println("\nProceso finalizado con éxito");
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
   
-  private static void insertRecords (Connection conn, File importFile) {
+  private static Integer[] insertRecords (Connection conn) {
+    List<String> orderIds = new ArrayList<>();
     String query =
-            "INSERT INTO `order`" +
+            "INSERT IGNORE INTO `order`" +
             "(`region`, `country`, `item_type`, `sales_channel`, `order_priority`," +
             "`order_date`, `order_id`, `ship_date`, `units_sold`, `unit_price`, " +
             "`unit_cost`, `total_revenue`, `total_cost`, `total_profit`)" +
             " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     
-    try (BufferedReader reader = Files.newBufferedReader(importFile.toPath());) {
-      PreparedStatement ps = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
-      conn.setAutoCommit(false);
-      
+    System.out.println("Iniciando lectura del fichero " + importFile.toPath());
+    try (BufferedReader fileReader = Files.newBufferedReader(importFile.toPath())) {
+      PreparedStatement ps = conn.prepareStatement(query,
+                                                   PreparedStatement.RETURN_GENERATED_KEYS);
       String line;
       int count = 0;
+      List<Integer> generatedKeys = new ArrayList<>();
+      ResultSet rs;
       
-      // Salta la línea de la cabecera antes de recorrer los datos
-      reader.readLine();
-      while ((line = reader.readLine()) != null) {
+      // Salta la línea de la cabecera
+      fileReader.readLine();
+      
+      System.out.println("Iniciada transacción");
+      while ((line = fileReader.readLine()) != null) {
         String[] record = line.split(",");
+        
+        orderIds.add(record[6]);
         
         setPreparedStatement(ps, record);
         
         if (count % BATCH_SIZE == 0) {
           ps.executeBatch();
-          System.out.println("Registros insertados: " + count);
+          rs = ps.getGeneratedKeys();
+          
+          while (rs.next()) {
+            generatedKeys.add(rs.getInt("GENERATED_KEY"));
+          }
+          
+          System.out.println("Registros procesados: " + count);
         }
         count++;
-        System.out.println(count);
       }
       ps.executeBatch();
-      System.out.println("Registros insertados: " + count);
-      conn.setAutoCommit(true);
-      System.out.println("Finalizada copia a la base de datos");
+      rs = ps.getGeneratedKeys();
+      
+      while (rs.next()) {
+        generatedKeys.add(rs.getInt("GENERATED_KEY"));
+      }
+      
+      System.out.println("Registros procesados: " + count);
+      System.out.println("\nFinalizada lectura del fichero\n\n");
+      System.out.println("Se han encontrado " + (count - generatedKeys.size()) + " duplicados");
+      
+      return !generatedKeys.isEmpty() ?
+             new Integer[] {generatedKeys.get(0),
+                            generatedKeys.get(generatedKeys.size() - 1)} : null;
     } catch (SQLException | IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  private static void generateSummary (Connection conn, Integer[] keys) {
+    String[] summaryFields = new String[] {"region", "country", "item_type",
+                                           "sales_channel", "order_priority"};
+    
+    for (String field : summaryFields) {
+      String query = String.format("SELECT `%s` AS `value`, COUNT(*) AS 'quantity' " +
+                                   "FROM `order` " +
+                                   "WHERE `id` BETWEEN %d AND %d " +
+                                   "GROUP BY `%s`", field, keys[0], keys[1], field);
+      
+      try (Statement statement = conn.createStatement()) {
+        statement.execute(query);
+        ResultSet rs = statement.getResultSet();
+        System.out.println(field.toUpperCase());
+        while (rs.next()) {
+          System.out.println("\t" + rs.getString("value") + ": " +
+                             rs.getInt("quantity"));
+        }
+        System.out.println();
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+  
+  private static void exportToFile (Connection conn, Integer[] keys) {
+    String query = String.format(
+            "SELECT " +
+            " `order_id`, `order_priority`, `order_date`, `region`, `country`, " +
+            " `item_type`, `sales_channel`, `ship_date`, `units_sold`, `unit_price`, " +
+            " `unit_cost`, `total_revenue`, `total_cost`, `total_profit` " +
+            "FROM `order` " +
+            "WHERE `id` BETWEEN %d AND %d " +
+            "ORDER BY `order_id`", keys[0], keys[1]);
+    
+    try (Statement statement = conn.createStatement()) {
+      ResultSet rs = statement.executeQuery(query);
+      List<String> dataToExport = getCSVDataToExport(rs);
+      
+      System.out.println("Exportando datos a archivo");
+      try (FileWriter fw = new FileWriter(exportFile)) {
+        for (String record : dataToExport) {
+          fw.write(record);
+        }
+        
+        System.out.println("Archivo creado correctamente");
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    } catch (SQLException e) {
       throw new RuntimeException(e);
     }
   }
@@ -88,9 +181,9 @@ public class App {
       ps.setString(3, record[2]);
       ps.setString(4, record[3]);
       ps.setString(5, record[4]);
-      ps.setObject(6, DataTreatment.parseDate(record[5]));
+      ps.setObject(6, Tools.parseDate(record[5]));
       ps.setString(7, record[6]);
-      ps.setObject(8, DataTreatment.parseDate(record[7]));
+      ps.setObject(8, Tools.parseDate(record[7]));
       ps.setInt(9, Integer.parseInt(record[8]));
       ps.setDouble(10, Double.parseDouble(record[9]));
       ps.setDouble(11, Double.parseDouble(record[10]));
@@ -99,75 +192,40 @@ public class App {
       ps.setDouble(14, Double.parseDouble(record[13]));
       ps.addBatch();
     } catch (SQLException e) {
+      System.out.println("add batch");
       throw new RuntimeException(e);
     }
   }
   
-  private static void generateSummary (Connection conn) {
-    String[] fields = new String[] {"region", "country", "item_type",
-                                    "sales_channel", "order_priority"};
-    for (String field : fields) {
-      String query = String.format("SELECT `%s` AS `field`, COUNT(*) AS 'quantity'" +
-                                   " FROM `order`" +
-                                   " GROUP BY `%s`", field, field);
-      try (Statement statement = conn.createStatement()) {
-        statement.execute(query);
-        ResultSet rs = statement.getResultSet();
-        System.out.println(field.toUpperCase());
-        while(rs.next()){
-          System.out.println("\t" + rs.getString("field") + ": " +
-                             rs.getInt("quantity"));
-        }
-        System.out.println();
-      } catch (SQLException e) {
-        throw new RuntimeException(e);
-      }
+  private static List<String> getCSVDataToExport (ResultSet rs) throws SQLException {
+    List<String> dataToExport = new ArrayList<>();
+    
+    dataToExport.add("Order ID,Order Priority,Order Date,Region,Country," +
+                     "Item Type,Sales Channel,Ship Date,Units Sold,Unit Price," +
+                     "Unit Cost,Total Revenue,Total Cost,Total Profit");
+    
+    while (rs.next()) {
+      String csvRecord =
+              String.format(
+                      "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,",
+                      rs.getString("order_id"),
+                      rs.getString("order_priority"),
+                      Tools.getFormattedDate(
+                              rs.getDate("order_date").toLocalDate()),
+                      rs.getString("region"),
+                      rs.getString("country"),
+                      rs.getString("item_type"),
+                      rs.getString("sales_channel"),
+                      Tools.getFormattedDate(
+                              rs.getDate("ship_date").toLocalDate()),
+                      rs.getInt("units_sold"),
+                      rs.getDouble("unit_price"),
+                      rs.getDouble("unit_cost"),
+                      rs.getDouble("total_revenue"),
+                      rs.getDouble("total_cost"),
+                      rs.getDouble("total_profit"));
+      dataToExport.add(csvRecord);
     }
+    return dataToExport;
   }
 }
-
-// List<Integer> generatedKeys = new ArrayList<Integer>();
-
-//generatedKeys.addAll(Arrays.asList());
-
-
-//      ResultSet rs = ps.getGeneratedKeys();
-//      while(rs.next()){
-//        System.out.println(rs.getString("GENERATED_KEY"));
-//      }
-
-//      List<List<String>> records =
-//              reader.lines()
-//                    .map(line -> Arrays.asList(line.split(",")))
-//                    .toList();
-
-
-//      String region = record[0];
-//      String country = record[1];
-//      String itemType = record[2];
-//      String salesChannel = record[3];
-//      String orderPriority = record[4];
-//      LocalDate orderDate = DataTreatment.parseDate(record[5]);
-//      String orderId = record[6];
-//      LocalDate shipDate = DataTreatment.parseDate(record[7]);
-//      int unitsSold = Integer.parseInt(record[8]);
-//      double unitPrice = Double.parseDouble(record[9]);
-//      double unitCost = Double.parseDouble(record[10]);
-//      double totalRevenue = Double.parseDouble(record[11]);
-//      double totalCost = Double.parseDouble(record[12]);
-//      double totalProfit = Double.parseDouble(record[13]);
-
-//      System.out.println(region);
-//      System.out.println(country);
-//      System.out.println(itemType);
-//      System.out.println(salesChannel);
-//      System.out.println(orderPriority);
-//      System.out.println(DataTreatment.showFormattedDate(orderDate));
-//      System.out.println(orderId);
-//      System.out.println(DataTreatment.showFormattedDate(shipDate));
-//      System.out.println(unitsSold);
-//      System.out.println(unitPrice);
-//      System.out.println(unitCost);
-//      System.out.println(totalRevenue);
-//      System.out.println(totalCost);
-//      System.out.println(totalProfit);
--- src/main/java/Tools/Tools.java
+++ src/main/java/Tools/Tools.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 
-public class DataTreatment {
+public class Tools {
   public static LocalDate parseDate (String date) {
     DateTimeFormatterBuilder dtfb = new DateTimeFormatterBuilder()
             .append(DateTimeFormatter
@@ -22,41 +22,39 @@ public class DataTreatment {
                                     "[M/d/uu]"));
     return LocalDate.parse(date, dtfb.toFormatter());
   }
-
-//  public static LocalDate parseDate (String date) {
-//    int currYearLastDigits = Year.now().getValue() % 100;
-//    String[] monthDayYear = date.split("/");
-//
-//    // Al tratarse de pedidos a digitalizar se asume que las fechas son pasadas
-//    // si el año no incluye las 4 cifras
-//    // se revisa para ver si se trata de 19xx o de 20xx
-//    // Esta lógica funciona con pedidos de menos de 100 años
-//    String newStrDate =
-//            (monthDayYear[2].length() == 2 ?
-//             (currYearLastDigits >= Integer.parseInt(monthDayYear[2]) ?
-//              "20" + monthDayYear[2] : "19" + monthDayYear[2]) :
-//             monthDayYear[2])
-//                    + "-" +
-//                    (monthDayYear[0].length() == 1 ? "0" + monthDayYear[0] :
-//                     monthDayYear[0])
-//                    + "-" +
-//                    (monthDayYear[1].length() == 1 ? "0" + monthDayYear[1] :
-//                     monthDayYear[1]);
-//
-//    return LocalDate.parse(newStrDate);
-//  }
   
   public static String getFormattedDate (LocalDate date) {
     return date.format(DateTimeFormatter.ofPattern("dd/MM/uuuu"));
   }
   
-  public static File selectFile () {
+  public static File selectImportFile () {
     JFileChooser chooser = new JFileChooser();
-    FileNameExtensionFilter filter = new FileNameExtensionFilter(
-            "Comma separated values", "CSV", "csv");
+    FileNameExtensionFilter filter =
+            new FileNameExtensionFilter("*.CSV", "CSV", "csv");
     chooser.setFileFilter(filter);
+    chooser.setDialogTitle("Abrir fichero para importar");
     int returnVal = chooser.showOpenDialog(null);
     return returnVal == JFileChooser.APPROVE_OPTION ?
            chooser.getSelectedFile() : null;
+  }
+  
+  public static File selectSaveFile () {
+    JFileChooser chooser = new JFileChooser();
+    FileNameExtensionFilter filter =
+            new FileNameExtensionFilter("*.CSV", "CSV", "csv");
+    chooser.setFileFilter(filter);
+    chooser.setDialogTitle("Elegir fichero para exportar");
+    int returnVal = chooser.showSaveDialog(null);
+    
+    File file = null;
+    if (returnVal == JFileChooser.APPROVE_OPTION){
+      file = chooser.getSelectedFile();
+      
+      String fileName = file.getName();
+      if (!fileName.matches(".csv&")){
+        file = new File(file.getAbsolutePath() + ".csv");
+      }
+    }
+    return file;
   }
 }
